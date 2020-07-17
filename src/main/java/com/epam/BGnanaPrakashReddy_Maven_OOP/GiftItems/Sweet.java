ea2c2c20a45ae5fde1980055cff01054dcd3cb6b
Gnana Prakash Reddy <gnanaprakash2505@gmail.com>
2020-07-17 15:46:47 +0530

    Initial Commit Maven Project

---
+++ src/main/java/com/epam/BGnanaPrakashReddy_Maven_OOP/GiftItems/Sweet.java
@@ -0,0 +1,61 @@
+package com.epam.BGnanaPrakashReddy_Maven_OOP.GiftItems;
+
+import java.util.*;
+import com.epam.BGnanaPrakashReddy_Maven_OOP.OOP.*;
+
+public class Sweet extends Gift{
+
+	public Sweet() {
+		chocolates = new ArrayList<Chocolate>();
+		candies = new ArrayList<Candie>();
+	}
+	
+	public void addChocolates(ArrayList<Chocolate> chocolates) {
+		this.chocolates = chocolates;
+	}
+	
+	public void addCandies(ArrayList<Candie> candies) {
+		this.candies = candies;
+	}
+	
+	public void addTotalWeight(double weight) {
+		this.totalWeight += weight;
+	}
+	
+	public void addTotalCost(double cost) {
+		this.totalCost += cost;
+	}
+	
+	public ArrayList<Chocolate> getChocolates() {
+		return chocolates;
+	}
+	
+	public ArrayList<Candie> getCandies() {
+		return candies;
+	}
+	
+	double getCost() {
+		return cost;
+	}
+	
+	public double getTotalCost() {
+		return totalCost;
+	}
+	
+	public double getTotalWeight() {
+		return totalWeight;
+	}
+	
+	public void printChocolates() {
+		for(Chocolate choco: chocolates) {
+			System.out.println(choco.toString());
+		}
+	}
+	
+	public void printCandies() {
+		for(Candie candie: candies) {
+			System.out.println(candie.toString());
+		}
+	}
+
+}
---
+++ src/main/java/com/epam/BGnanaPrakashReddy_Maven_OOP/OOP/Gift.java
@@ -0,0 +1,15 @@
+package com.epam.BGnanaPrakashReddy_Maven_OOP.OOP;
+
+import java.util.ArrayList;
+import com.epam.BGnanaPrakashReddy_Maven_OOP.GiftItems.*;
+
+
+public abstract class Gift {
+	public String name;
+	public double cost;
+	public double weight;
+	public double totalWeight;
+	public double totalCost;
+	public ArrayList<Chocolate> chocolates;
+	public ArrayList<Candie> candies;
+}
